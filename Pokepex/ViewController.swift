56a594f88dd028193c8f95943f5f9fdd3141dc1f
Jordy <graciamancilla@outlook.com>
2022-02-03 18:20:10 -0600

    Initial Commit

---
+++ Pokepex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokepex
+//
+//  Created by Jordy Gracia on 03/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
